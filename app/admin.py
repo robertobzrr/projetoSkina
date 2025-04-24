11934b316e461e2e70986f23d2cfad1fa50eb52d
robertobzrr <robertobezerrafh@outlook.com>
2025-04-24 19:33:00 -0300

    alteração chaveProduto

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Categoria, Produtos, Lista, Lista_Produtos
+from .models import Categoria, Produtos, Lista, Lista_Produtos, Categoria_Produto
 
 
 admin.site.register(Categoria)
 admin.site.register(Produtos)
 admin.site.register(Lista)
 admin.site.register(Lista_Produtos)
+admin.site.register(Categoria_Produto)
 
--- app/models.py
+++ app/models.py
@@ -5,16 +5,26 @@ class Categoria(models.Model):
     cod_cat = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome 
+
 
 class Produtos(models.Model):
     cod_prod = models.IntegerField(primary_key=True)
-    cod_cat = models.ForeignKey(Categoria, on_delete=models.CASCADE, db_column="cod_cat")
+   # cod_cat = models.ForeignKey(Categoria, on_delete=models.CASCADE, db_column="cod_cat")
     nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
 
 
 class Lista(models.Model):
     cod_list = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=100)
+   # produtos = models.ManyToManyField(Produtos, through="Lista_produtos")
+
+    def __str__(self):
+        return self.nome     
 
 
 class Lista_Produtos(models.Model):
@@ -23,7 +33,20 @@ class Lista_Produtos(models.Model):
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["cod_prod", "cod_list"], name="CompositePrimaryKey")
+            models.UniqueConstraint(fields=["cod_prod", "cod_list"], name="Lista_Produtos_CompositePrimaryKey")
         ]
 
+
+
+class Categoria_Produto(models.Model):
+    cod_cat = models.ForeignKey(Categoria, on_delete=models.CASCADE, db_column="cod_cat")
+    cod_prod = models.ForeignKey(Produtos, on_delete=models.CASCADE, db_column="cod_prod")
+    nome = models.CharField(max_length=100)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["cod_cat", "cod_prod"], name="Categoria_Produto_CompositePrimaryKey")
+        ]
     
+    def __str__(self):
+        return self.nome 
